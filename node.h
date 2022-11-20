b562debfd35bc11af775366d91eb7b4dc84e457f
Olivier2Carglass <104084572+oliver2Carglass@users.noreply.github.com>
2022-11-21 00:50:30 +0100

    Update node.h

--- node.h
+++ node.h
@@ -1,23 +1,19 @@
-//
-// Created by raphb on 06/11/2022.
-//
-
 #ifndef PROJET_GENERATEUR_DE_MOTS_NODE_H
 #define PROJET_GENERATEUR_DE_MOTS_NODE_H
 
-#define TAILLE_MAX 100
+#define size_word 100
 #define ALPHABET_SIZE 26
 
 typedef struct s_variation
 {
-    char word[TAILLE_MAX]; //mot fléchi
-    char variation_bloc[TAILLE_MAX];
+    char word[size_word]; //mot fléchi
+    char variation_bloc[size_word];
 } t_variation,* p_variation;
 
 typedef struct s_end_word
 {
-    char base_word[TAILLE_MAX]; //mot basique
-    char base_bloc[TAILLE_MAX]; //temps et genre du mot de base
+    char base_word[size_word]; //mot basique
+    char base_bloc[size_word]; //temps et genre du mot de base
     int type;  //type de mot
     int nb_variations; //nombre de mots fléchis dérivant de ce mot
     p_variation variations_word; //Tableau dynamique des variations à déclarer
@@ -33,8 +29,7 @@ p_end_word end_word;
 struct s_letter_node * sons; //Tableau dynamique des fils à déclarer
 } t_letter_node, * p_letter_node ;
 
-p_letter_node CreateNode(char c);
-void DisplayNode(t_letter_node n);
-int AddSon(p_letter_node p,char c);
+p_letter_node Create_Node(char c);
+int Add_Son(p_letter_node p, char c);
 
 #endif //PROJET_GENERATEUR_DE_MOTS_NODE_H
